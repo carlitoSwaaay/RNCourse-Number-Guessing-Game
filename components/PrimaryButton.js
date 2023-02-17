20c8a815a1870e12fbbd1f4e71c193f3b1600b32
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-17 15:46:45 -0800

    Update PrimaryButton.js

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -4,24 +4,40 @@ const primaryButton = ({ children }) => {
 
   const pressHandler = () => {
     console.log("pressed");
+
   };
   return (
-    <Pressable onPress={pressHandler}>
-      <View style={styles.container}>
-      <Text>{children}</Text>
-    </View>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={styles.buttonInnerContainer}
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
     </Pressable>
+    </View>
   );
 };
 
 export default primaryButton;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#72063c",
+  buttonOuterContainer: {
     borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+
+  },
+  buttonInnerContainer: {
+    // borderRadius: 28,
+    backgroundColor: "#72063c",
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
   },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+
 });
