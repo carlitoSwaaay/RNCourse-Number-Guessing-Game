07c0234c4a666af911831ed424d9e5826587bb5f
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-17 14:16:53 -0800

    Update PrimaryButton.js

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,5 +1,12 @@
-const primaryButton () => {
+import { Text } from "react-native";
+import { View } from "react-native";
 
+const primaryButton = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
 };
 
 export default primaryButton;
