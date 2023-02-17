dbef9d8728ee040de7c6721aba4532a50dbffc59
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-17 15:03:10 -0800

    Update StartGameScreen.js
    more styling updates to number input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-    <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
   </View>
@@ -25,6 +25,17 @@ const styles = StyleSheet.create({
     shadowColor: 'black',
     shadowOffset: { width: 1, height: 2 },
     shadowRadius: 8,
-    shadowOpacity: 0.26,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
   },
 });
