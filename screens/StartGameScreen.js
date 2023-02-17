148f901e844469abbaddcdde0e99c9ca32ba568c
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-17 14:17:36 -0800

    Update StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,14 @@
 import { TextInput, View } from 'react-native';
 
+import PrimaryButton from '../components/PrimaryButton';
+
 const StartGameScreen = () => {
-  return
+  return (
   <View>
     <TextInput />
+      <PrimaryButton>Start Game</PrimaryButton>
   </View>
+  );
 };
 
 export default StartGameScreen;
