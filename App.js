f28456b4f883f6cafbfff9509b0a0c83ed391d2c
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-17 14:25:56 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet } from 'react-native';
+
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
