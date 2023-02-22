32f3b27f95feb2522d438b700e8a68e5d11e9a73
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-22 13:50:08 -0800

    Update StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 
@@ -11,8 +11,20 @@ const StartGameScreen = () => {
     setEnteredNumber(enteredText);
   };
 
-  const confirmInputHandler = () => {
+  const resetInputHandler = () => {
+    setEnteredNumber('');
+  };
 
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
+    }
   };
 
   return (
@@ -28,7 +40,7 @@ const StartGameScreen = () => {
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton >Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler} >Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputHandler} >Confirm</PrimaryButton>
