170701bed55827c265388baad096fec365323383
carlitoSwaaay <cacollier02@yahoo.com>
2023-03-01 11:53:48 -0800

    added NumberContainer
    also changed imports on colors

---
+++ components/game/NumberContainer.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+
+const NumberContainer = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default NumberContainer;
