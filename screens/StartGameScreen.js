a60b2a2ba687961c26907ae63ad060b44d51e6b2
carlitoSwaaay <cacollier02@yahoo.com>
2023-02-28 19:39:21 -0800

    Update StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -80,13 +80,13 @@ const styles = StyleSheet.create({
   numberInput: {
     height: 50,
     width: 50,
-    textAlign: 'center',
     fontSize: 32,
     borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
     color: Colors.accent500,
     marginVertical: 8,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   buttonsContainer: {
     flexDirection: 'row',
