f819d6cb5ea9629454302d86d43d11dc3ee1107b
carlitoSwaaay <cacollier02@yahoo.com>
2023-03-01 12:00:21 -0800

    Update NumberContainer.js
    styling

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,12 +1,31 @@
+import { StyleSheet } from "react-native";
 import { Text, View } from "react-native";
+import Colors from "../../constants/colors";
 
 
-const NumberContainer = () => {
+const NumberContainer = ({ children }) => {
   return (
     <View>
-      <Text></Text>
+      <Text>{children}</Text>
     </View>
   );
 };
 
 export default NumberContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  numberText: {
+    color: Colors.accent500,
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+});
