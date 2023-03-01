3e373d50b324b24f160457d7db8a35371856adb1
carlitoSwaaay <cacollier02@yahoo.com>
2023-03-01 15:15:13 -0800

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,9 @@
 import react, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Colors from '../constants/colors';
 
 import Title from '../components/ui/Title';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
 
 const generateRandomBetween = (min, max, exclude) => {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -20,14 +20,22 @@ const GameScreen = ({ userNumber }) => {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
+  const nextGuessHandler = (direction) => {
+    if (direction === 'lower') {
+      generateRandomBetween()
+    }
+  };
+
   return (
     <View style={styles.screen}>
       <Title style={styles.title}>Opponent's guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
-        {/* +
-        - */}
+        <View>
+          <PrimaryButton onPress={ }>+</PrimaryButton>
+          <PrimaryButton onPress={ }>-</PrimaryButton>
+        </View>
       </View>
       <View>{/*LOG ROUNDS*/}</View>
     </View>
