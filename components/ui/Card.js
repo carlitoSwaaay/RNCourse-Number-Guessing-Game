399c2ec16d3cdd393f8851525ee95267b1d701ab
carlitoSwaaay <cacollier02@yahoo.com>
2023-03-10 11:35:42 -0800

    Update Card.js

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,5 +1,27 @@
-const Card = () => {
-  return
+import { StyleSheet } from "react-native";
+
+
+const Card = ({ children }) => {
+  return <View style={styles.inputContainer}>
+    {children}
+  </View>
 };
 
 export default Card;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 36,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: Colors.primary800,
+    elevation: 12,
+    shadowColor: 'black',
+    shadowOffset: { width: 1, height: 2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.25,
+  },
+});
