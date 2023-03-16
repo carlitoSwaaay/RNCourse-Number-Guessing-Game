e3e4e7f73273367054d595e03fc4bf815b23256e
carlitoSwaaay <cacollier02@yahoo.com>
2023-03-16 14:05:27 -0700

    added useEffect..
    handler for end game and restart

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -38,6 +38,11 @@ const GameScreen = ({ userNumber, onGameOver }) => {
     }
   }, [currentGuess, userNumber, onGameOver]);
 
+  useEffect(() => {
+    minBoundary = 1;
+    maxBoundary = 100;
+  }, []);
+
   const nextGuessHandler = (direction) => {
     if (
       (direction === 'lower' && currentGuess < userNumber) ||
