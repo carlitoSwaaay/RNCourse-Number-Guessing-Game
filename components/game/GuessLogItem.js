598e19c22b0bae7f1aeb696d03de21396b3301cb
carlitoSwaaay <cacollier02@yahoo.com>
2023-03-23 16:15:19 -0700

    Update GuessLogItem.js

--- components/game/GuessLogItem.js
+++ components/game/GuessLogItem.js
@@ -1,5 +1,34 @@
-const GuessLogItem() {
-  return
+import { Text, View, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+
+
+
+
+const GuessLogItem = ({ roundNumber, guess }) => {
+  return <View>
+    <Text>#{roundNumber}</Text>
+    <Text>Opponent's Guess: {guess}</Text>
+  </View>
 }
 
-export default GuessLogItem;
+export default GuessLogItem;.
+
+const styles = StyleSheet.create({
+  listItem: {
+    borderColor: Colors.primary800,
+    borderWidth: 1,
+    borderRadius: 40,
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: Colors.accent500,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+});
