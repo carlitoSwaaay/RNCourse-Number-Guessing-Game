a37bf5e247872c250e99e386e706a2fe4a7a4398
carlitoSwaaay <cacollier02@yahoo.com>
2023-04-02 18:17:28 -0700

    Update NumberContainer.js
    corrected typo

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -21,15 +21,15 @@ const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: deviceWidth > 380 ? 12 : 24,
-    margin: deviceWidth > 380 ? 12 : 24,
+    padding: deviceWidth < 380 ? 12 : 24,
+    margin: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: deviceWidth > 380 ? 28 : 36,
+    fontSize: deviceWidth < 380 ? 28 : 36,
     fontFamily: 'open-sans-bold',
   },
 });
